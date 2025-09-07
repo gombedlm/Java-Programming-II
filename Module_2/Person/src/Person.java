8262ca8fe098399ab117830ec2dd0da75d116dc4
Layken Gombeda <gombedlm@mail.uc.edu>
2025-09-07 17:29:20 -0400

    Refactoring again to clean up code structure

---
+++ Module_2/Person/src/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+    public static void main(String[] args) {
+        // Reads person records from a file into an array list and supplies the list
+        String firstname;
+        String lastname;
+        String id
+    }
+}
